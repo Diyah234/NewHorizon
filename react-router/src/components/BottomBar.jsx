9565ed3149d5b6b9154d9a39c8ad97ccec0597af
Diyah234 <rodiyahabudu@gmail.com>
2022-12-20 01:38:29 +0100

    Abudu Rodiyah

---
+++ react-router/src/components/BottomBar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function BottomBar() {
+    return (
+        <footer>
+            {/* Fill in your actual details here */}
+            <ul>
+                <li>Name: Abudu Rodiyah</li>
+                <li>Matric Number: 21/0875</li>
+                <li>Department:computer information system</li>
+            </ul>
+        </footer>
+    );
+}
+
+export default BottomBar;
